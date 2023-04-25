a4632090748e56541200357434f45da5ac9e68f6
jaywant lugun <jaywantlugun2000@gmail.com>
2023-04-25 22:44:55 +0530

    first commit

---
+++ app/src/main/java/com/example/a040_memory_game/models/UserImageList.kt
@@ -0,0 +1,6 @@
+package com.example.a040_memory_game.models
+
+import com.google.firebase.firestore.PropertyName
+
+
+data class UserImageList(@PropertyName("images") val images: List<String>? = null)
